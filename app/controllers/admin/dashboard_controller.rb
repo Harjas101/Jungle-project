d2ad3357a070974561ac6855b8b7901f29d30f8c
Harjas101 <anand.harjas@gmail.com>
2022-07-12 18:03:04 -0400

    implement admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   include HttpAuthConcern
 
   def show
+    @products_count = Product.count
+    @products_quantity =Product.sum(:quantity)
+    @categories = Category.count
   end
 end
